eeeddefe81f8df4c4f816195c2d851927017a66d
Glitch (slash-toothsome-titanoceratops) <none>
2022-07-15 20:35:44 -0500

    yeah

--- routes/thanks.js
+++ routes/thanks.js
@@ -7,6 +7,7 @@ router.get("/thanks", function (req, res, next) {
     if(!yeah || yeah === "") res.redirect("/");
     res.render("thanks", {
         title: "Thanks for your payment",
+        description: "Thanks for your payment",
         message: "Your payment has been processed successfully",
         keywords: "donate, help, help us, help us keep our website up and running, any api, api, api's, api's for your needs, Any Bot",
         host: req.headers.host,
